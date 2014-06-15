821beae430805c378b68bf70f5e9a9beb0cdc628
Sun Sophie <sunsophie@Suns-MacBook-Pro.local>
2014-06-14 23:40:24 -0400

    activity 2

---
+++ wk2/activity2/main.js
@@ -0,0 +1,8 @@
+(function($){
+	var allPanels = $('.accordion > dd').hide();
+	$('.accordion > dt >a').click(function(){
+		allPanels.slideUp();
+		$(this).parent().next().slideDown();
+		return false
+	});
+})(jQuery);
